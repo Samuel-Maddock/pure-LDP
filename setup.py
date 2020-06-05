ad8ff5e40db83cc3845d5b5992bc432369563e36
Samuel Maddock <samuel-maddock@hotmail.com>
2020-06-05 21:08:51 +0100

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pure-ldp',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=["xxhash", "numpy", "scipy"],
     url='https://github.com/Samuel-Maddock/pure-LDP',
