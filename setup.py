018fbd715f6ef83c436774a410e766242358fcb5
Samuel Maddock <samuel-maddock@hotmail.com>
2020-08-16 20:01:43 +0100

    Version 1.0.5

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
+import shutil
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+shutil.rmtree("./build")
+shutil.rmtree("./dist")
+shutil.rmtree("./pure_ldp.egg-info")
+
 setup(
     name='pure-ldp',
-    version='1.0.4',
-    packages=find_packages(include=['pure_ldp'], exclude=['pure_ldp.development', ]),
+    version='1.0.5',
+    packages=find_packages(exclude=['*development*', "*apple_sf*", "*treehistogram*", "*rappor*",
+                                    "*priv_count_sketch*", "*hashtogram*", "*explicit_hist*", ]),
     install_requires=["xxhash", "numpy", "scipy", "bitstring", "bitarray"],
     url='https://github.com/Samuel-Maddock/pure-LDP',
-    license='',
+    license='MIT',
     author='Samuel Maddock',
     author_email='samuel-maddock@hotmail.com',
     description='Simple pure LDP frequency oracle implementations',
