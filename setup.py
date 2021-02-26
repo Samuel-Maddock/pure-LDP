e80f0ab5dabd57f90fe50a24dd3b627eb7176676
Samuel Maddock <samuel-maddock@hotmail.com>
2021-02-26 22:15:02 +0000

    Updated setup.py version to 1.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if os.path.exists("./pure_ldp.egg-info"):
 
 setup(
     name='pure-ldp',
-    version='1.0.6',
+    version='1.1.0',
     packages=find_packages(exclude=['*development*', "*apple_sf*", "*treehistogram*", "*rappor*",
                                     "*priv_count_sketch*", "*hashtogram*", "*explicit_hist*", ]),
     install_requires=["xxhash", "numpy", "scipy", "bitstring", "bitarray"],
